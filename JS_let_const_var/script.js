e769aa6986c3fe78ac955a62d132efe54fa166b9
Jorge Zarek Saleme Gomez <zareksaleme14@gmail.com>
2025-07-03 15:29:20 -0600

    Initial commit: practice with let, const, and var

---
+++ JS_let_const_var/script.js
@@ -0,0 +1,13 @@
+/*Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+Try to change one of the changed variables now, and observe what happens.*/
+
+let language = "Spanish";
+const country = "Mexico";
+const flag = "Mexican Flag";
+const isCold = false;
+console.log (isCold);
+
+//If I try to change or mutate a const value it will mark an error because this key word does not expect to change never.
