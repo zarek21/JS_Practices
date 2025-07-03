cb760fd4b67d8f378ec0173a563ec3206949eb0c
Jorge Zarek Saleme Gomez <zareksaleme14@gmail.com>
2025-07-03 15:31:37 -0600

    Initial commit: practice with values and variables

---
+++ JS_Values_Variables/script.js
@@ -0,0 +1,5 @@
+//Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+//Log their values to the console.
+
+let country = "Mexico", continent = "America", population = 100000000;
+console.log(country, continent, population);
