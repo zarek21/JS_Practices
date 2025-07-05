70d39cba43f54864b183d5c3894be93691beaa2c
Jorge Zarek Saleme Gomez <zareksaleme14@gmail.com>
2025-07-05 02:07:14 -0600

    Initial commit: Ternary Operator Practice

---
+++ JS_Ternary_Operator/script.js
@@ -0,0 +1,8 @@
+// If your country's population is greater than 33 million, use the ternary operator to log a string like this to the console: "Portugal's population is above average". Otherwise, simply log "Portugal's population is below average". Notice how only one word change between these two sentences!
+
+// After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
+
+const mexicoPopulation = 130000000;
+const averagePopulation = 33000000;
+const message = mexicoPopulation >= averagePopulation ? 'Mexican population is above average' : 'Mexican population is below average';
+console.log(population);
