c604580826f757345f936938e8605cef118c92fd
Jorge <jorge.saleme77@gmail.com>
2025-08-13 17:03:42 -0600

    feat: finish objects practice

--- JS_Objects/script.js
+++ JS_Objects/script.js
@@ -1,29 +1,78 @@
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+
+// console.log(jonas);
+
+// console.log(jonas.lastName);
+
+// console.log(jonas.lastName);
+// console.log(jonas["lastName"]);
+
+// brackets difference
+// const nameKey = "Name";
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
+
+// const interestedIn = prompt(
+//   "what do you want to know about Jonas? choose between firstName, lastName,age, job and friends?"
+// );
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log("Wrong request");
+// }
+
+// jonas.location = "Portugal";
+// jonas["twitter"] = "@bitZarek";
+// console.log(jonas);
+
+//CHALLENGE
+
+//Jonas has 3 friends, and his best friend is called 'Michael'
+
+// console.log(
+//   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+// );
+
+// const fullName = jonas.firstName + " " + jonas.lastName;
+// console.log(fullName);
+
 const jonas = {
   firstName: "Jonas",
   lastName: "Schmedtmann",
-  age: 2037 - 1991,
+  birthYear: 1991,
   job: "teacher",
   friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // }
+
+  // calcAge: function () {
+  //   return 2037 - this.birthYear;
+  // }
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      jonas.job
+    }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+  },
 };
 
-console.log(jonas);
+console.log(jonas.calcAge());
 
-console.log(jonas.lastName);
+//Jonas is a 46-year old teacher, and he has a driver's license
 
-console.log(jonas.lastName);
-console.log(jonas["lastName"]);
-
-// brackets difference
-const nameKey = "Name";
-console.log(jonas["first" + nameKey]);
-console.log(jonas["last" + nameKey]);
-
-const interestedIn = prompt(
-  "what do you want to know about Jonas? choose between firstName, lastName,age, job and friends?"
-);
-
-if (jonas[interestedIn]) {
-  console.log(jonas[interestedIn]);
-} else {
-  console.log("Wrong request");
-}
+console.log(jonas.getSummary());
