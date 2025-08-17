72699f846cd3675002c40c48a701339d3c3ed9cd
Jorge <jorge.saleme77@gmail.com>
2025-08-17 15:22:08 -0600

    add new practice files

---
+++ JS_Problem_Solving/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+///////////////////////////////////////
+// Using Google, StackOverflow and MDN
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
