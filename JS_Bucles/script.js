af73fa793097a49df3307eaf1118dfd5841b20ba
Jorge <jorge.saleme77@gmail.com>
2025-08-17 13:20:20 -0600

    feat: add more functions and practice more

--- JS_Bucles/script.js
+++ JS_Bucles/script.js
@@ -80,17 +80,46 @@
 //   console.log(`Lifting weights repetition ${rep}`);
 // }
 
-let rep = 1;
-while (rep <= 10) {
-  console.log(`WHILE: Lifting weights repetition ${rep}`);
-  rep++;
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`WHILE: Lifting weights repetition ${rep}`);
+//   rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log("Loop is about to end...");
+// }
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
 }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
+console.log(bills, tips, totals);
 
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log("Loop is about to end...");
-}
+const calcAvergae = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAvergae([2, 3, 7]));
+console.log(calcAvergae(totals));
+console.log(calcAvergae(tips));
