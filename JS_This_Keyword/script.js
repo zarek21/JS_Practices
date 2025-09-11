e6357a332e47c5649e415e848eedfa083321f5a5
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-11 14:32:35 -0600

    :sparkles: finsih this keywoerd practice

--- JS_This_Keyword/script.js
+++ JS_This_Keyword/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const calcAge = function (birthYear) {
+  console.log(2025 - birthYear);
+  // console.log(this);
+};
+
+calcAge(2002);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2025 - birthYear);
+  // console.log(this);
+};
+
+calcAgeArrow(2010);
+
+const zarek = {
+  year: 2002,
+  calcAge: function () {
+    console.log(this);
+    console.log(2025 - this.year);
+  },
+};
+zarek.calcAge();
+
+const matilda = {
+  year: 2010,
+};
+
+matilda.calcAge = zarek.calcAge;
+matilda.calcAge();
+
+const f = zarek.calcAge;
+f();
