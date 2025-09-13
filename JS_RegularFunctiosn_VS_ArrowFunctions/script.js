2e475a34b336c0e33ae20fb94eb566b5cd489031
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-13 12:59:03 -0600

    :sparkles: finish practice

--- JS_RegularFunctiosn_VS_ArrowFunctions/script.js
+++ JS_RegularFunctiosn_VS_ArrowFunctions/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const zarek = {
+  firstName: "Zarek",
+  year: 2002,
+
+  calcAge: function () {
+    console.log(this);
+    console.log(2025 - this.year);
+
+    // Solution 1
+    const self = this;
+    const isMillenial = function () {
+      console.log(self.year >= 1981 && self.year <= 1996);
+    };
+    isMillenial();
+
+    // Solution 2
+    // Here we use an arrow function
+    // so the this keyword gets the value
+    // inhereted from it's parent, which is
+    // this case it is from the calc methon object zarek
+    const isMillenialArrow = () => {
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenialArrow();
+  },
+
+  // greet: () => console.log(`Hey, ${this.firstName}`), // NEVER USE ARROW FUNCTIONS ON OBJECTS
+  greet: function () {
+    console.log(`Hey ${this.firstName}!`);
+  },
+};
+zarek.greet();
+zarek.calcAge();
