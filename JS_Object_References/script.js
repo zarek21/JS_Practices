77fa0d033a5b32293e7e0aef1439cf188a189af7
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-15 16:38:04 -0600

    finish practice

--- JS_Object_References/script.js
+++ JS_Object_References/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+const jessica1 = {
+  fistName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+};
+
+function marryPerson(originalPerson, newLastName) {
+  originalPerson.lastName = newLastName;
+  return originalPerson;
+}
+
+const marriedJessica = marryPerson(jessica1, "Davis");
+
+// const marriedJessica = jessica1;
+// marriedJessica.lastName = "Davis";
+
+console.log("BEFORE:", jessica1);
+console.log("AFTER:", marriedJessica);
+
+const jessica = {
+  fistName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+  family: ["Alice", "Bob"],
+};
+
+const jessicaCopy = { ...jessica };
+jessicaCopy.lastName = "Davis";
+
+console.log(jessica, jessicaCopy);
+
+// Deep copy / clone
+const jessicaClone = structuredClone(jessica);
+jessicaClone.family.push("Mary");
+jessicaClone.family.push("John");
+
+console.log("ORIGINAL:", jessica);
+console.log("CLONE:", jessicaClone);
