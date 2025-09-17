33f10ee8b68727c09730cb5d3aa7819bdbd83e1d
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-16 21:07:21 -0600

    :sparkles: add practice

--- JS_Destructuring_Arrays/script.js
+++ JS_Destructuring_Arrays/script.js
@@ -7,3 +7,28 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 };
+
+// without destructuring
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// SWITCHING VARIABLES
+
+// WITHOUT DESTRUCTURING
+const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary);
+
+// USING DESTRUCTURING
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
