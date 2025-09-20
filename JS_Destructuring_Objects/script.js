6b4e34429ad9a7632785577b99f298d197d9fab9
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-20 14:08:08 -0600

    :sparkles: finish practice

--- JS_Destructuring_Objects/script.js
+++ JS_Destructuring_Objects/script.js
@@ -1,1 +1,69 @@
 "use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 23,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// but what if we want the variable name
+// to be different from the propertie name
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// And what if we want to set 'DEFAULT' values for properties
+// which does not exist with that name on the object
+// THIS HELP US TO NOT RECEIVE ERRORS OR BUGS BECAUSE OF UNDEFINED VALUE
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
+
+// And what if I want to mutate variables while destructuring objects
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested Objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
