870d8b11487948b487823190d85e18929671e6b9
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-22 17:03:13 -0600

    :sparkles: finish practice

--- JS_Spread_Operator/script.js
+++ JS_Spread_Operator/script.js
@@ -1,1 +1,73 @@
 "use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 23,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
+  },
+};
+
+// THIS WITHOUT SPREAD OPERATOR
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+// WITH SPREAD OPERATOR
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+// COPY ARRAY
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join Arrays
+const joinedArrays = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(joinedArrays);
+
+// IT ALSO WORKS WITH ITERABLES ( STRINGS, MAPS, SETS, ARRAYS)
+const str = "Jonas";
+const letters = [...str, "", "s"];
+console.log(letters);
+console.log(...str);
+
+// REAL WORLD EXAMPLE
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt("Ingredient 2?"),
+//   prompt("Ingredient 3?"),
+// ];
+// console.log(ingredients);
+// restaurant.orderPasta(...ingredients);
+
+// OBJECTS
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: "Guiseppe" };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
