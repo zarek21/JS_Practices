a67d7a7c6c2f512c1074fbbdf67e934c945b5cae
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-23 09:30:43 -0600

    :sparkles: practice more spread operator

--- JS_Spread_Operator/script.js
+++ JS_Spread_Operator/script.js
@@ -1,58 +1,58 @@
 "use strict";
 
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 23,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
+// const restaurant = {
+//   name: "Classico Italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 23,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0,
+//       close: 24,
+//     },
+//   },
 
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
-  },
-};
+//   orderPasta: function (ing1, ing2, ing3) {
+//     console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
+//   },
+// };
 
 // THIS WITHOUT SPREAD OPERATOR
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
 
 // WITH SPREAD OPERATOR
-const newArr = [1, 2, ...arr];
-console.log(newArr);
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
 
-console.log(...newArr);
-console.log(1, 2, 7, 8, 9);
+// console.log(...newArr);
+// console.log(1, 2, 7, 8, 9);
 
-const newMenu = [...restaurant.mainMenu, "Gnocci"];
-console.log(newMenu);
+// const newMenu = [...restaurant.mainMenu, "Gnocci"];
+// console.log(newMenu);
 
 // COPY ARRAY
-const mainMenuCopy = [...restaurant.mainMenu];
+// const mainMenuCopy = [...restaurant.mainMenu];
 
 // Join Arrays
-const joinedArrays = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(joinedArrays);
+// const joinedArrays = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(joinedArrays);
 
 // IT ALSO WORKS WITH ITERABLES ( STRINGS, MAPS, SETS, ARRAYS)
-const str = "Jonas";
-const letters = [...str, "", "s"];
-console.log(letters);
-console.log(...str);
+// const str = "Jonas";
+// const letters = [...str, "", "s"];
+// console.log(letters);
+// console.log(...str);
 
 // REAL WORLD EXAMPLE
 // const ingredients = [
@@ -64,10 +64,254 @@ console.log(...str);
 // restaurant.orderPasta(...ingredients);
 
 // OBJECTS
-const newRestaurant = { foundedIn: 1998, ...restaurant, founder: "Guiseppe" };
-console.log(newRestaurant);
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: "Guiseppe" };
+// console.log(newRestaurant);
 
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = "Ristorante Roma";
-console.log(restaurantCopy.name);
-console.log(restaurant.name);
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = "Ristorante Roma";
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
+// PRACTICE MORE
+const books = [
+  {
+    title: "Algorithms",
+    author: ["Robert Sedgewick", "Kevin Wayne"],
+    publisher: "Addison-Wesley Professional",
+    publicationDate: "2011-03-24",
+    edition: 4,
+    keywords: [
+      "computer science",
+      "programming",
+      "algorithms",
+      "data structures",
+      "java",
+      "math",
+      "software",
+      "engineering",
+    ],
+    pages: 976,
+    format: "hardcover",
+    ISBN: "9780321573513",
+    language: "English",
+    programmingLanguage: "Java",
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Structure and Interpretation of Computer Programs",
+    author: [
+      "Harold Abelson",
+      "Gerald Jay Sussman",
+      "Julie Sussman (Contributor)",
+    ],
+    publisher: "The MIT Press",
+    publicationDate: "2022-04-12",
+    edition: 2,
+    keywords: [
+      "computer science",
+      "programming",
+      "javascript",
+      "software",
+      "engineering",
+    ],
+    pages: 640,
+    format: "paperback",
+    ISBN: "9780262543231",
+    language: "English",
+    programmingLanguage: "JavaScript",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ["Randal E. Bryant", "David Richard O'Hallaron"],
+    publisher: "Prentice Hall",
+    publicationDate: "2002-01-01",
+    edition: 1,
+    keywords: [
+      "computer science",
+      "computer systems",
+      "programming",
+      "software",
+      "C",
+      "engineering",
+    ],
+    pages: 978,
+    format: "hardcover",
+    ISBN: "9780130340740",
+    language: "English",
+    programmingLanguage: "C",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Operating System Concepts",
+    author: ["Abraham Silberschatz", "Peter B. Galvin", "Greg Gagne"],
+    publisher: "John Wiley & Sons",
+    publicationDate: "2004-12-14",
+    edition: 10,
+    keywords: [
+      "computer science",
+      "operating systems",
+      "programming",
+      "software",
+      "C",
+      "Java",
+      "engineering",
+    ],
+    pages: 921,
+    format: "hardcover",
+    ISBN: "9780471694663",
+    language: "English",
+    programmingLanguage: "C, Java",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: "Engineering Mathematics",
+    author: ["K.A. Stroud", "Dexter J. Booth"],
+    publisher: "Palgrave",
+    publicationDate: "2007-01-01",
+    edition: 14,
+    keywords: ["mathematics", "engineering"],
+    pages: 1288,
+    format: "paperback",
+    ISBN: "9781403942463",
+    language: "English",
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "The Personal MBA: Master the Art of Business",
+    author: "Josh Kaufman",
+    publisher: "Portfolio",
+    publicationDate: "2010-12-30",
+    keywords: ["business"],
+    pages: 416,
+    format: "hardcover",
+    ISBN: "9781591843528",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: "Crafting Interpreters",
+    author: "Robert Nystrom",
+    publisher: "Genever Benning",
+    publicationDate: "2021-07-28",
+    keywords: [
+      "computer science",
+      "compilers",
+      "engineering",
+      "interpreters",
+      "software",
+      "engineering",
+    ],
+    pages: 865,
+    format: "paperback",
+    ISBN: "9780990582939",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: "Deep Work: Rules for Focused Success in a Distracted World",
+    author: "Cal Newport",
+    publisher: "Grand Central Publishing",
+    publicationDate: "2016-01-05",
+    edition: 1,
+    keywords: ["work", "focus", "personal development", "business"],
+    pages: 296,
+    format: "hardcover",
+    ISBN: "9781455586691",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
+
+// 1.0 Each book object has the author property, which stores an array
+// of strings (author names) if there are multiple authors, or a single
+// string (author name) if there is just one author.
+// Declare an array called bookAuthors, and fill it with authors
+// of the first two books from the books array.
+// The bookAuthors array should have just one level (no nested arrays).
+const bookAuthors = [...books[0].author, books[1].author];
+console.log(bookAuthors);
+
+// 1.1 Write a function called spellWord that accepts a single string
+// as an argument. This function should log to the console
+// each letter of the argument separated by a space.
+function spellWord(string) {
+  console.log(...string);
+}
+
+spellWord("Javascript");
