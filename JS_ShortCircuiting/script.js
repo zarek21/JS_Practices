7ef9c7dacaa9cb7516f89d081d78eaffeb4da284
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-28 12:28:50 -0600

    :sparkles: practice more

--- JS_ShortCircuiting/script.js
+++ JS_ShortCircuiting/script.js
@@ -33,33 +33,276 @@ const restaurant = {
   },
 };
 
-console.log("---------OR-----------");
+// console.log("---------OR-----------");
 // use Any data type, return ANY data type, short-circuiting
-console.log(3 || "Zarek"); // PRINT 3 BECAUSE IT´S THE FIRST TRUTHY VALUE
-console.log("" || "Zarek"); // PRINT ZAREK BECAUSE EMPTY STRING IS FALSE
-console.log(true || 0); // PRINT TRUE BECASUE IT´S FIRST TRUTHY VALUE
-console.log(undefined || null); // BOTH are falsy, so it returns the last falsy value
+// console.log(3 || "Zarek"); // PRINT 3 BECAUSE IT´S THE FIRST TRUTHY VALUE
+// console.log("" || "Zarek"); // PRINT ZAREK BECAUSE EMPTY STRING IS FALSE
+// console.log(true || 0); // PRINT TRUE BECASUE IT´S FIRST TRUTHY VALUE
+// console.log(undefined || null); // BOTH are falsy, so it returns the last falsy value
 
 // PRINTS HELLO because it´s the first TRUTHY value
-console.log(undefined || 0 || "" || "Hello" || 23 || null);
+// console.log(undefined || 0 || "" || "Hello" || 23 || null);
 
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
 
-console.log("---------AND-----------");
-console.log(0 && "Zarek "); // DEVUELVE 0 POR QUE ES PRIMER FALSY QUE ENCUENTRA
-console.log(7 && "Zarek"); // RETURNS Zarek because it´s the last truthy that it´s declared
+// console.log("---------AND-----------");
+// console.log(0 && "Zarek "); // DEVUELVE 0 POR QUE ES PRIMER FALSY QUE ENCUENTRA
+// console.log(7 && "Zarek"); // RETURNS Zarek because it´s the last truthy that it´s declared
 
 // Practical Example
-if (restaurant.orderPizza) {
-  restaurant.orderPizza("Mushrooms", "Spinach", "Tomatoe");
-}
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza("Mushrooms", "Spinach", "Tomatoe");
+// }
 
-restaurant.orderPizza &&
-  restaurant.orderPizza("Mushrooms", "Spinach", "Tomatoe");
+// restaurant.orderPizza &&
+//   restaurant.orderPizza("Mushrooms", "Spinach", "Tomatoe");
 
 // Practice More
+const books = [
+  {
+    title: "Algorithms",
+    author: ["Robert Sedgewick", "Kevin Wayne"],
+    publisher: "Addison-Wesley Professional",
+    publicationDate: "2011-03-24",
+    edition: 4,
+    keywords: [
+      "computer science",
+      "programming",
+      "algorithms",
+      "data structures",
+      "java",
+      "math",
+      "software",
+      "engineering",
+    ],
+    pages: 976,
+    format: "hardcover",
+    ISBN: "9780321573513",
+    language: "English",
+    programmingLanguage: "Java",
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Structure and Interpretation of Computer Programs",
+    author: [
+      "Harold Abelson",
+      "Gerald Jay Sussman",
+      "Julie Sussman (Contributor)",
+    ],
+    publisher: "The MIT Press",
+    publicationDate: "2022-04-12",
+    edition: 2,
+    keywords: [
+      "computer science",
+      "programming",
+      "javascript",
+      "software",
+      "engineering",
+    ],
+    pages: 640,
+    format: "paperback",
+    ISBN: "9780262543231",
+    language: "English",
+    programmingLanguage: "JavaScript",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ["Randal E. Bryant", "David Richard O'Hallaron"],
+    publisher: "Prentice Hall",
+    publicationDate: "2002-01-01",
+    edition: 1,
+    keywords: [
+      "computer science",
+      "computer systems",
+      "programming",
+      "software",
+      "C",
+      "engineering",
+    ],
+    pages: 978,
+    format: "hardcover",
+    ISBN: "9780130340740",
+    language: "English",
+    programmingLanguage: "C",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Operating System Concepts",
+    author: ["Abraham Silberschatz", "Peter B. Galvin", "Greg Gagne"],
+    publisher: "John Wiley & Sons",
+    publicationDate: "2004-12-14",
+    edition: 10,
+    keywords: [
+      "computer science",
+      "operating systems",
+      "programming",
+      "software",
+      "C",
+      "Java",
+      "engineering",
+    ],
+    pages: 921,
+    format: "hardcover",
+    ISBN: "9780471694663",
+    language: "English",
+    programmingLanguage: "C, Java",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: "Engineering Mathematics",
+    author: ["K.A. Stroud", "Dexter J. Booth"],
+    publisher: "Palgrave",
+    publicationDate: "2007-01-01",
+    edition: 14,
+    keywords: ["mathematics", "engineering"],
+    pages: 1288,
+    format: "paperback",
+    ISBN: "9781403942463",
+    language: "English",
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "The Personal MBA: Master the Art of Business",
+    author: "Josh Kaufman",
+    publisher: "Portfolio",
+    publicationDate: "2010-12-30",
+    keywords: ["business"],
+    pages: 416,
+    format: "hardcover",
+    ISBN: "9781591843528",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: "Crafting Interpreters",
+    author: "Robert Nystrom",
+    publisher: "Genever Benning",
+    publicationDate: "2021-07-28",
+    keywords: [
+      "computer science",
+      "compilers",
+      "engineering",
+      "interpreters",
+      "software",
+      "engineering",
+    ],
+    pages: 865,
+    format: "paperback",
+    ISBN: "9780990582939",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: "Deep Work: Rules for Focused Success in a Distracted World",
+    author: "Cal Newport",
+    publisher: "Grand Central Publishing",
+    publicationDate: "2016-01-05",
+    edition: 1,
+    keywords: ["work", "focus", "personal development", "business"],
+    pages: 296,
+    format: "hardcover",
+    ISBN: "9781455586691",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
+
+// 1.0 Some of the book objects have the programmingLanguage property,
+// which specifies what programming language is used in the book,
+// for example Write a function called hasExamplesInJava that takes a book
+// object from the books array as an argument. This function should return
+// true if the book uses Java, or a string 'no data available' if it uses
+// other language or no programming language at all.
+function hasExamplesInJava(book) {
+  return book.programmingLanguage === "Java" || "no data available";
+}
+console.log(hasExamplesInJava(books[1]));
+
+// 1.1 Some of the book objects have the onlineContent property, which is
+// either true or false. Loop over the books array, and for the books that
+// provide online content, log to the console a string in this format:
+// "${title}" provides online content. Use short-circuiting.
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent && console.log(`${books[i].title}`);
+}
