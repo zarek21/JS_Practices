084e94eae60b05af7f197a9389b8332a140c98d6
Zarek Saleme <jorge.saleme77@gmail.com>
2025-10-06 12:47:10 -0600

    finish practice

--- JS_Enhaced_ObjectLiterals/script.js
+++ JS_Enhaced_ObjectLiterals/script.js
@@ -1,31 +1,40 @@
 "use strict";
 
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 23,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0,
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 23,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  // ES6 enhanced object literals
+  openingHours,
+
+  // WITHOUT ENHANCED OBJECT LITERALS
+  // openingHours:openingHours,
+
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
   },
 
-  orderPizza: function (mainIgredient, ...otherIngredients) {
+  orderPizza(mainIgredient, ...otherIngredients) {
     console.log(
       `The main ingredient of your pizza is ${mainIgredient} and the extra ingredients are ${otherIngredients}`
     );
