a830df1c5e44a9d126e54cf892590305bf2b4cae
Zarek Saleme <jorge.saleme77@gmail.com>
2025-10-06 13:20:49 -0600

    :sparkles: finished practicing more

--- JS_Enhaced_ObjectLiterals/script.js
+++ JS_Enhaced_ObjectLiterals/script.js
@@ -41,3 +41,36 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+// PRACTICE MORE
+// 1.0 Below is the bookData array that contains other arrays. Each inner
+// array consists of the property name (first element), and the value
+// (second element). For example, in
+// ['title', 'Computer Networking: A Top-Down Approach'], 'title' is the
+// property name, and 'Computer Networking: A Top-Down Approach' is meant
+// to be the value assigned to that property name.Using computed properties,
+// fill the newBook object with the properties and values from the bookData
+// array. The first one is done already.
+const bookData = [
+  ["title", "Computer Networking: A Top-Down Approach"],
+  ["author", ["James F. Kurose", "Keith W. Ross"]],
+  ["publisher", "Addison Wesley"],
+];
+
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+
+// 1.1 Below is the pages variable. Add it as a property of the newBook2
+// object. Use the shorter way.
+const pages = 880;
+
+const newBook2 = {
+  title: "The C Programming Language",
+  author: ["Brian W. Kernighan", "Dennis M. Ritchie"],
+  pages,
+};
+console.log(newBook2);
