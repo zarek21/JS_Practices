230423689655af725a7a48610657d277a345ea69
Zarek Saleme <jorge.saleme77@gmail.com>
2025-10-11 15:12:24 -0600

    :sparkles: finish practicing

--- JS_LoopingObjects_KeyValues/script.js
+++ JS_LoopingObjects_KeyValues/script.js
@@ -1,1 +1,66 @@
 "use strict";
+
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 23,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0,
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  // ES6 enhanced object literals
+  openingHours,
+
+  // WITHOUT ENHANCED OBJECT LITERALS
+  // openingHours:openingHours,
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
+  },
+
+  orderPizza(mainIgredient, ...otherIngredients) {
+    console.log(
+      `The main ingredient of your pizza is ${mainIgredient} and the extra ingredients are ${otherIngredients}`
+    );
+    console.log(otherIngredients);
+  },
+};
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+// LOOPING over properties names [ also named 'keys']
+for (const day of Object.keys(openingHours)) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// PROPERTY VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
