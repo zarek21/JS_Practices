8bfcfbc879fc1316df11a6517b0df831e06ab86d
Zarek Saleme <jorge.saleme77@gmail.com>
2025-10-11 17:09:10 -0600

    :sparkles: finished practicing

--- Challenge_2/script.js
+++ Challenge_2/script.js
@@ -40,3 +40,27 @@ const game = {
     team2: 6.5,
   },
 };
+
+// 1.0 Loop over the game.scored array and print each player name to the
+// console, along with the goal number (Example: "Goal 1: Lewandowski")
+for (const [index, player] of game.scored.entries())
+  console.log(`Goal ${index + 1}: ${player}`);
+
+// 1.1 Use a loop to calculate the average odd and log it to the console
+// (we already studied how to calculate averages, you can go check if you don't remember)
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) {
+  average += odd;
+}
+average /= odds.length;
+console.log(average);
+
+// 1.2 Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
